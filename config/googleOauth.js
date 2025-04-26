0f4489af4db5549eb526cda57e26273dac214161
Your Name <your-email@example.com>
2025-04-27 03:43:17 +0530

    done

--- config/googleOauth.js
+++ config/googleOauth.js
@@ -7,11 +7,7 @@ passport.use(
   new GoogleStrategy ({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-<<<<<<< HEAD
     callbackURL : process.env.GOOGLE_CALLBACK_URL
-=======
-    callbackURL : "http://localhost:3000/auth/google/callback"
->>>>>>> 3f8818e96dc400d52041b267b3de8d4e1d1fa80e
   },(accessToken , refreshToken , profile,done) => {
       console.log('Google profile:', profile);
       return done(null, profile);
