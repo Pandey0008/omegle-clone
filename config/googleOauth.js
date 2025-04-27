08a63882adb628178c1710725f200d8ddc99c427
Your Name <your-email@example.com>
2025-04-27 13:28:31 +0530

    render edit

--- config/googleOauth.js
+++ config/googleOauth.js
@@ -2,12 +2,20 @@ const passport = require('passport');
 require('dotenv').config()
 const GoogleStrategy = require('passport-google-oauth20');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const CLIENT_URL = isProduction
+  ? "https://omegle-clone-gq94.onrender.com"
+  : process.env.BASE_URL;
+
+const GOOGLE_CALLBACK_URL = `${CLIENT_URL}/auth/google/callback`;
+
 
 passport.use(
   new GoogleStrategy ({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL : process.env.GOOGLE_CALLBACK_URL
+    callbackURL : GOOGLE_CALLBACK_URL
   },(accessToken , refreshToken , profile,done) => {
       console.log('Google profile:', profile);
       return done(null, profile);
